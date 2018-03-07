b08835da87559c0215acbda157f4f65f1d740148
z-jack <emailjiong@126.com>
2018-03-07 22:46:05 +0800

    Construction of tag matrix.

--- Spider.py
+++ Spider.py
@@ -1,15 +1,16 @@
-from bs4 import BeautifulSoup as BS
 from urllib.parse import urljoin, urlparse, urlunparse
+from bs4 import BeautifulSoup as BS
 from WebParser import WebParser
 from itertools import chain
 import requests
 import Utils
+import json
 import os
 
 
 class Spider:
     def __init__(self, url=None, path=None):
-        self._url = "http://view.sdu.edu.cn/"
+        self._url = "http://view.sdu.edu.cn/index.htm"
         self.set_url(url)
         self._host = "view.sdu.edu.cn"
         if Utils.is_path_exists_or_creatable(path):
@@ -21,25 +22,41 @@ class Spider:
         self._queue = []
         self._viewed = []
         self._counter = 0
+        self._matrix = {}
+        self._map = {}
 
-    def _save_web(self, txt):
+    def _parse_web(self, txt):
         web = WebParser()
-        web.analyse(txt, self._url)
+        web.id = self._counter
+        tags = web.analyse(txt, self._url)
+        self._map[web.id] = [self._url, web.title]
+        for tag in tags:
+            if self._matrix.get(tag, None) is None:
+                self._matrix[tag] = []
+            self._matrix[tag].append(web.id)
 
     def _prefetch(self):
         if self._url:
             return requests.head(self._url).headers['content-type'] == 'text/html'
         return False
 
+    def _decode(self, string, codecs=None):
+        if codecs is None:
+            codecs = ['utf8', 'gb18030', 'gbk', 'gb2312', 'big5']
+        for i in codecs:
+            try:
+                return string.decode(i)
+            except UnicodeDecodeError:
+                pass
+
     def _fetch(self):
         if self._prefetch():
             res = requests.get(self._url)
             self._viewed.append(self._url)
-            if res.status_code == 200:
+            if 200 <= res.status_code < 300:
                 self._counter += 1
-                print('%d: %s' % (self._counter, self._url))
-                html = res.content.decode('utf8')
-                self._save_web(html)
+                html = self._decode(res.content)
+                self._parse_web(html)
                 root = BS(html, 'lxml')
                 for link in root.find_all('a'):
                     url = urljoin(self._url, link.get('href'))
@@ -55,10 +72,13 @@ class Spider:
         if Utils.is_valid_url(url):
             self._url = url
 
-    def start(self):
+    def start(self, web_count=-1):
         self._queue = []
         self._fetch()
-        while len(self._queue):
+        while len(self._queue) and web_count != 0:
+            web_count -= 1
+            if web_count < 0:
+                web_count += 1
             self._url = self._queue.pop(0)
             self._fetch()
 
@@ -66,7 +86,15 @@ class Spider:
         self.set_url(url)
         self.start()
 
+    def save(self):
+        with open(os.path.join(self._path, 'tag_matrix.json'), 'w+') as f:
+            f.write(json.dumps(self._matrix))
+        with open(os.path.join(self._path, 'url_mapping.json'), 'w+') as f:
+            f.write(json.dumps(self._map))
+
 
 if __name__ == '__main__':
     sp = Spider()
     sp.start()
+    sp.save()
+    print('finish')
--- WebParser.py
+++ WebParser.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup as BS
-from bs4 import UnicodeDammit
-from bs4.element import Comment
 from urllib.parse import urlparse
+from bs4.element import Comment
+from bs4 import ResultSet
 import jieba.analyse
+import requests
+import re
 
 
 class WebParser:
     def __init__(self):
         self.title = "无标题"
         self.id = -1
+        self.url = 'about:blank'
         self._tags = []
 
     def __repr__(self):
@@ -19,27 +22,88 @@ class WebParser:
 
     @staticmethod
     def _filter(element):
-        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        if element.parent.name in ['select', 'input', 'textarea', 'style', 'script', 'head', 'title', 'meta',
+                                   '[document]']:
             return False
         if isinstance(element, Comment):
             return False
         return True
 
-    def analyse(self, txt: str, url: str):
-        parsed_url = urlparse(url)
-        root = BS(txt)
-        if parsed_url.path.startswith('/info/'):
-            article = root.find('form', attrs={"name": "_newscontent_fromname"})
-            self.title = article.find('h3').get_text()
-            content = article.find('div', attrs={'class': 'news_content'})
-            content = content.find_all(text=True)
-            content = filter(self._filter, content)
-            content = '\n'.join(content)
-            self._tags = jieba.analyse.extract_tags(content, topK=50)
+    def _filter_and_tag(self, content):
+        if isinstance(content, ResultSet):
+            x_content = None
+            for element in content:
+                if x_content:
+                    x_content += element.find_all(text=True)
+                else:
+                    x_content = element.find_all(text=True)
+            content = x_content
         else:
-            self.title = root.title.get_text()
-            content = root.find_all(text=True)
-            content = filter(self._filter, content)
-            content = '\n'.join(content)
-            self._tags = jieba.analyse.extract_tags(content, topK=50)
+            content = content.find_all(text=True)
+        content = filter(self._filter, content)
+        content = self.title + '\n' + '\n'.join(content)
+        self._tags = jieba.analyse.extract_tags(content, topK=50)
+
+    def _naive_analyse(self, root, url):
+        main_body = root.find('div', attrs={'class': 'main_navbg'})
+        if not main_body:
+            print('lack template in url<%s>' % url)
+            self._filter_and_tag(root)
+        else:
+            main_body = main_body.find_next('div', attrs={'class': 'le'})
+            if not main_body:
+                print('lack template in url<%s>' % url)
+                self._filter_and_tag(root)
+            else:
+                self._filter_and_tag(main_body)
+
+    def analyse(self, txt: str, url: str):
+        self.url = url
+        parsed_url = urlparse(url)
+        root = BS(txt, 'lxml')
+        try:
+            if parsed_url.path.startswith('/info/'):
+                article = root.find('form', attrs={"name": "_newscontent_fromname"})
+                self.title = article.find('h3').get_text()
+                self._filter_and_tag(article.find('div', attrs={'class': 'news_content'}))
+            elif parsed_url.path.startswith('/sdrw/'):
+                main_body = root.find('div', attrs={'id': 'top'}).find_all_next('div', attrs={
+                    'id': re.compile('wrap\d+')})
+                self.title = root.find('div', attrs={'id': 'wrap_pos'}).find('h3').find_all('a')[-1].get_text()
+                self._filter_and_tag(main_body)
+            else:
+                path_arr = parsed_url.path.split('/')
+                self.title = root.title.get_text()
+                if len(path_arr) == 2:
+                    if path_arr[-1] in ['', 'index.htm']:
+                        main_body = root.find('div', attrs={'class': 'w1000'}).find_all_previous('div',
+                                                                                                 attrs={
+                                                                                                     'class': 'w1012'})
+                        self._filter_and_tag(main_body)
+                    elif path_arr[-1] == 'sdrw.htm':
+                        main_body = root.find('div', attrs={'id': 'top'}).find_all_next('div', attrs={
+                            'id': re.compile('wrap\d+')})
+                        self._filter_and_tag(main_body)
+                    elif path_arr[-1] == 'xssdjt.htm':
+                        main_body = root.find('div', attrs={'class': 'wrap'}).find('div', attrs={'class': None})
+                        self._filter_and_tag(main_body)
+                    elif path_arr[-1] in ['ypx.htm', 'zpx.htm']:
+                        pageid = {
+                            'ypx.htm': 125532,
+                            'zpx.htm': 125531
+                        }
+                        res = requests.post('http://view.sdu.edu.cn/system/resource/js/news/hotdynpullnews.jsp',
+                                            data={'owner': 1251758245, 'viewid': pageid[path_arr[-1]],
+                                                  'actionmethod': 'getnewslist'})
+                        if 200 <= res.status_code < 300:
+                            json = res.json()
+                            json = map(lambda x: x['title'], json)
+                            self._tags = jieba.analyse.extract_tags('\n'.join(json), topK=50)
+                    else:
+                        self._naive_analyse(root, url)
+                else:
+                    self._naive_analyse(root, url)
+        except AttributeError:
+            self._naive_analyse(root, url)
+        self._tags = list(filter(lambda x: not re.compile('^(\.+)$').match(x), self._tags))
         return self._tags
